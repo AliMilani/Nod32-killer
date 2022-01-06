787d81612845f32ce02e86c82e88104eb54258aa
Ali Milani <59066012+AliMilani@users.noreply.github.com>
2022-01-06 15:15:33 +0330

    Initial release.

---
+++ nod32-killer.bat
@@ -0,0 +1,71 @@
+
+@echo off
+
+title Nod32 Killer
+
+@REM Author: Ali Milani Amin
+@REM Description: This script will remove all ESET Nod32 products from your computer.
+@REM Version: 1.0
+
+::::::::::::::::::::::::::::::::::::::::::::
+:: Elevate.cmd - Version 4
+:: Automatically check & get admin rights
+:: see "https://stackoverflow.com/a/12264592/1016343" for description
+::::::::::::::::::::::::::::::::::::::::::::
+ CLS
+ ECHO.
+@REM  ECHO =============================
+@REM  ECHO Running Admin shell
+@REM  ECHO =============================
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+@REM   ECHO **************************************
+@REM   ECHO Invoking UAC for Privilege Escalation
+@REM   ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ ::::::::::::::::::::::::::::
+ ::START
+ ::::::::::::::::::::::::::::
+ 
+ wmic product where name="ESET Security" call uninstall /nointeractive
+ exit
+
+
